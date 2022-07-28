b754bacc4c3c97dd2a8a1e09de94ab65ce6de768
soyoung <psy99326@gamil.com>
2022-07-29 00:25:17 +0900

    Add: 2579 (계단 오르기, DP)

---
+++ BAEKJOON/2579.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int dp[305][3];
+int score[305];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    for(int i=1; i<=n; i++) cin >> score[i];
+
+    dp[1][1] = score[1];
+    dp[2][1] = score[1] + score[2]; 
+    dp[2][2] = score[2]; 
+    
+    for(int i=3; i<=n; i++){
+        dp[i][1] = dp[i-1][2] + score[i];
+        dp[i][2] = max(dp[i-2][1], dp[i-2][2]) + score[i];
+    }
+
+    cout << max(dp[n][1], dp[n][2]);
+
+}
