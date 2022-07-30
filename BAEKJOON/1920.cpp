e508e90375d3e8d0e6b651b9f3b7426f7fc1f396
s0y0ung <psy99326@naver.com>
2022-07-30 23:25:48 +0900

    Add: 1920 (수 찾기 이분 탐색)

---
+++ BAEKJOON/1920.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[100005];
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+
+    for(int i=0; i<n; i++) cin >> a[i];
+    sort(a, a+n);
+
+    int m, target;
+    cin >> m;
+    while(m--){
+        cin >> target;
+        cout << binary_search(a, a+n, target) << "\n";
+    }
+
+}
